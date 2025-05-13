b93abf33474bd80cb2d7440eaa6ed0bb35266c2a
Zsolt Kiss <kissrudolfzsolt@gmail.com>
2025-05-13 18:45:53 +0200

    Add assigments

--- src/main/java/hu/soter/jbe/grades/FileProvider.java
+++ src/main/java/hu/soter/jbe/grades/FileProvider.java
@@ -3,16 +3,18 @@ package hu.soter.jbe.grades;
 import java.io.File;
 
 public interface FileProvider {
-
+    // ANGELA
     // Adja vissza a File-t.
     // Ha nem letezik, dobjon kivetelt.
     // NEVKONVENCIO: src/main/resources/2a_nevsor.txt
     File getNevsorFile(String osztaly) throws NevsorNemLetezikException;
 
+    // ATTILA
     // Hozza letre a file-t es adja vissza.
     // NEVKONVENCIO: src/main/resources/2a_nevsor.txt
     File createNevsorFile(String osztaly);
 
+    // BALINT
     // NEVKONVENCIO: src/main/resources/2a_biologia_20250508.txt
     File createErdemjegyFile(String osztaly, String tantargy);
 
